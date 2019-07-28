64d6f8f0469985a6ee17121e40f11866f816f78f
Ammar Ratnani <ammrat13@gmail.com>
2019-07-28 18:41:50 -0500

    SDDM and colors

--- .config/i3blocks/scripts/colors.sh
+++ .config/i3blocks/scripts/colors.sh
@@ -2,7 +2,7 @@
 
 # Defines the colors used by all the other scripts
 
-COLOR_NORMAL='#CFD8DC'
-COLOR_GOOD='#8BC34A'
-COLOR_DEGRADED='#FFC107'
-COLOR_BAD='#FF9800'
+COLOR_NORMAL='#6fa0c8'
+COLOR_GOOD='#a6c0d8'
+COLOR_DEGRADED='#ac9f9b'
+COLOR_BAD='#7d7a85'
